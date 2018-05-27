b97ad8a76c109d42c5a0aa6415bb84b7c9e067bb
Rômulo Göelzer Portolann <romulogoelzer@gmail.com>
2018-05-27 12:40:00 -0300

    Commit inicial

---
+++ src/test/java/com/hustik/pedidosapi/PedidosApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hustik.pedidosapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PedidosApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
