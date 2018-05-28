53c123869f0b2be9009a7bc9a87a0e5875e9c3a9
Rômulo Göelzer Portolann <romulogoelzer@gmail.com>
2018-05-27 21:01:28 -0300

    Categoria Resource, Service, Domain, config DB em memoria H2

---
+++ src/main/java/com/hustik/pedidosapi/domain/Categoria.java
@@ -0,0 +1,71 @@
+package com.hustik.pedidosapi.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Rômulo Göelzer Portolann
+ */
+@Entity
+public class Categoria implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+
+    public Categoria() {
+    }
+
+    public Categoria(Integer id, String nome) {
+        super();
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 67 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Categoria other = (Categoria) obj;
+        return Objects.equals(this.id, other.id);
+    }
+
+}
---
+++ src/main/java/com/hustik/pedidosapi/services/CategoriaService.java
@@ -0,0 +1,12 @@
+package com.hustik.pedidosapi.services;
+
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Rômulo Göelzer Portolann
+ */
+@Service
+public class CategoriaService {
+
+}
